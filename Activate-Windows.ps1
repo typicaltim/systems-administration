d6f7d57e11629a79bed3b446faf9133b3c063606
tim <typicaltim@gmail.com>
2020-12-03 12:26:07 -0800

    Create Activate-Windows.ps1

---
+++ Activate-Windows.ps1
@@ -0,0 +1,12 @@
+# Set KMS Server
+C:\windows\system32\slmgr.vbs //B /skms <# KMS SERVER #>
+
+# Install the product key
+C:\windows\system32\slmgr.vbs //B /ipk <# SERIAL NUMBER #>
+
+# Activate the product key
+C:\windows\system32\slmgr.vbs //B /ato
+
+# Clear the key from the registry
+# Not using this right now but wanted to save it here
+# C:\windows\system32\slmgr.vbs //B /cpky
